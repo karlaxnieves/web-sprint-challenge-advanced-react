b5d68120c93c1913bcf3290a875927b9eff17aa7
Karla Nieves <karlaxnieves@gmail.com>
2021-05-07 08:36:23 -0700

    second test was done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -10,4 +11,31 @@ test("form header renders", () => {
     expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render (<CheckoutForm/>);
+    const nameSelected = "Karla";
+
+    const nameInput = screen.getByLabelText(/first name:/i);
+    userEvent.type(nameInput, nameSelected);
+
+    const lastInput = screen.getByLabelText(/last name:/i);
+    userEvent.type(lastInput, "Nieves");
+
+    const addressInput = screen.getByLabelText(/address:/i);
+    userEvent.type(addressInput, "111 N. Lambda");
+
+    const cityInput = screen.getByLabelText(/city:/i);
+    userEvent.type(cityInput, "Los Angeles");
+
+    const stateInput = screen.getByLabelText(/state:/i);
+    userEvent.type(stateInput, "California");
+
+    const zipInput = screen.getByLabelText(/zip:/i);
+    userEvent.type(zipInput, "11111");
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    const confirmMessageDisplay = screen.queryByText(nameSelected, lastInput, addressInput, cityInput, stateInput, zipInput);
+    expect(confirmMessageDisplay).toBeInTheDocument();
+});
